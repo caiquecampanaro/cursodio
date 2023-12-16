102c4c86bc588aab6a3d26261daf85263bd52c8f
caiquecampanaro <caiquecampanaro@gmail.com>
2023-12-16 17:17:04 -0300

    estudo de regex

---
+++ estudos/regex.rb
@@ -0,0 +1,25 @@
+#regex - ex = expressões reg = regulares
+#notação para representar padrões em Strings
+#( telefone, cpf,etc)
+#ex: se o campo é válido, busca em texto, retirar
+#partes do texto
+###### MANEIRAS DE CRIAR REGEX#####
+#1. puts /abcdef/
+#2. %r{abcdef}
+#3.Regexp.new('expressao')
+
+#Operador de comparação =~
+# puts /by/ =~ 'ruby'
+# puts 'ruby' =~ /by/
+# puts 'ruby' =~ /rails/
+
+#Método match do Regex
+#phrase = "Olá, como vai você?"
+
+#match_data = /como/.match(phrase)
+#puts match_data #trás como 
+# puts match_data.pre_match #trás o inicio da palavra
+#puts match_data.post_match #trás o final com referencia
+# da palavra em match
+
+puts /\?/.match('Tudo bem?')
